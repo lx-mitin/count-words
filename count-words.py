e8675033b0dbbe30d70b9e39667a23fc10fc2f87
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-03-21 07:29:37 +0400

    add count-words.py

---
+++ count-words.py
@@ -0,0 +1,9 @@
+"""Count words."""
+
+def count_words(text):
+
+	# TODO: convert to lowercase
+	# TODO: split text into tokens (words), removing punctuation
+	# TODO: aggregate word counts to dictionary
+
+	raise NotImplementedError
