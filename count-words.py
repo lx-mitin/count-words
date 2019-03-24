522f5cff8c8dd1b9a90d605bd8977c1c0a140321
Aleksei Mitin <lx.mitin@gmail.com>
2019-03-24 07:35:32 +0400

    Update count-words.py
    add test run, implement count words

--- count-words.py
+++ count-words.py
@@ -1,9 +1,34 @@
-"""Count words."""
+"""Count words"""
+import string
+from collections import defaultdict
 
 def count_words(text):
 
-	# TODO: convert to lowercase
-	# TODO: split text into tokens (words), removing punctuation
-	# TODO: aggregate word counts to dictionary
+	t = text.casefold()	
+	t = t.translate(t.maketrans('','', string.punctuation))
+	t = t.split()
+	
+	cw = defaultdict(int)
+	for w in t:
+		cw[w] += 1
 
-	raise NotImplementedError
+	return cw
+
+def test_run():
+	from datetime import datetime 
+	with open('roadside-picnic-ru.txt','r') as f:
+		t = f.read()
+		print('Start counting time: {}'.format(datetime.now()))
+		words = count_words(t)
+		print('End counting time: {}'.format(datetime.now()))
+		
+		print('Total number of words: {}'.format(sum(words.values())))
+		print('Number of unique words: {}'.format(len(words)))
+		
+		sorted_counts = sorted(words.items(), key = lambda p: p[1], reverse = True)
+		print('10 most popular words are:')
+		for word, count in sorted_counts[:10]:
+			print('{}:\t{}'.format(word, count))
+
+if __name__ == '__main__':
+	test_run()
