74265128a43bdd5e92f6ae0e361741568fb24b2c
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-03-27 07:33:54 +0400

    use BeautifulSoup instead of manual parsing

--- fetch-web-page.py
+++ fetch-web-page.py
@@ -1,13 +1,13 @@
 '''Fetch a web page'''
 import requests
-import re
+from bs4 import BeautifulSoup 
 
 # Fetch a web page
 p = requests.get('https://github.com/lx-mitin?tab=repositories&q=&type=public')
 
 
-# Remove HTML tags using RegEx
-pattern = re.compile(r'<.*?>')
-t = pattern.sub('',p.text)
+# Remove HTML tags using BeatifulSoup library
+soup = BeautifulSoup(p.text)
+t = soup.get_text()
 
 print(t)
