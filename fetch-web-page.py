980c332faffb16965c5ec4d63bc9f6624d23150d
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-03-28 07:43:39 +0400

    Get list of repositories

--- fetch-web-page.py
+++ fetch-web-page.py
@@ -6,8 +6,9 @@ from bs4 import BeautifulSoup
 p = requests.get('https://github.com/lx-mitin?tab=repositories&q=&type=public')
 
 
-# Remove HTML tags using BeatifulSoup library
-soup = BeautifulSoup(p.text)
-t = soup.get_text()
+# Get list of repositories
+soup = BeautifulSoup(p.text,'lxml')
+reps = soup.body.find_all('a', itemprop="name codeRepository")
 
-print(t)
+for r in reps:
+	print('Repository: {} \t address: {}'.format(r.string, r.attrs['href']))
