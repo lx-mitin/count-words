2475f94f825a4747e54dec6e2379f42913bb8e1f
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-03-29 11:43:54 +0400

    Clean repository names and load repositories data into json file

--- fetch-web-page.py
+++ fetch-web-page.py
@@ -1,14 +1,19 @@
 '''Fetch a web page'''
-import requests
+from requests import get as requests_get
 from bs4 import BeautifulSoup 
+from json import dump as json_dump
+from os import getcwd
 
 # Fetch a web page
-p = requests.get('https://github.com/lx-mitin?tab=repositories&q=&type=public')
+p = requests_get('https://github.com/lx-mitin?tab=repositories&q=&type=public')
 
 
-# Get list of repositories
+# Get a list of repositories
 soup = BeautifulSoup(p.text,'lxml')
-reps = soup.body.find_all('a', itemprop="name codeRepository")
+rep_tags = soup.body.find_all('a', itemprop="name codeRepository")
+rep_names = [{'rep_name':r.string.strip(),'rep_url':'https://github.com'+r.attrs['href']} for r in rep_tags]
 
-for r in reps:
-	print('Repository: {} \t address: {}'.format(r.string, r.attrs['href']))
+with open(getcwd()+'/data/rep-names.json','w') as json_file:
+    json_dump(rep_names,json_file)
+
+print('Repositories data is loaded to `/data/rep-names.json` file')
