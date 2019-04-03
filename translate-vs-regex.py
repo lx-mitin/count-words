2ba86ce6cc2293ed5481583b129db59708f870d9
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-04-03 08:09:03 +0400

    process all files in specified folder

--- translate-vs-regex.py
+++ translate-vs-regex.py
@@ -3,16 +3,19 @@ from string import punctuation
 from re import sub
 from datetime import datetime
 import numpy as np
+from os import walk
 
 # replace punctuation characters with space by using regular expression
 def normalize_regex(text):
     
-    return sub(r'[^а-яА-Я0-9]',' ',text)
+    return sub(r'[^a-zA-Zа-яА-Я0-9]',' ',text)
 
-# replace punctuation characters with space by using regular expression (letter are preliminary lowered)
+# replace punctuation characters with space by using regular expression 
+# (letter are preliminary lowered)
 def lower_regex(text):
     
-    return sub(r'[^а-я0-9]',' ',text)
+    return sub(r'[^a-zа-я0-9]',' ',text)
+
 # replace punctuation characters with space by using translate
 def normalize_trans(text):
 
@@ -27,48 +30,69 @@ def remove_punctuation_translate(text):
 #  compare translate vs regex
 def translate_vs_regex_compare():
 
-    with open('roadside-picnic-ru.txt','r') as f:
+    results = []
+    for root, dirs, files in walk('./data-txt'):
 
-        text = f.read()
-        text = text.casefold()
+        for filename in files:
 
-    regex_times = []
-    lower_times = []
-    trans_times = []
-    remove_times = []
+            result = {}
+            result['name'] = filename
 
-    for i in range(10):
+            with open(root+'/'+filename,'r') as f:
 
-        time = datetime.now()
-        t1 = normalize_regex(text)
-        delta_time = datetime.now() - time
-        regex_times.append(delta_time.total_seconds())
+                text = f.read()
+            
+            result['length'] = len(text)
 
-        t2 = text.casefold()
-        time = datetime.now()
-        t2 = lower_regex(text)
-        delta_time = datetime.now() - time
-        lower_times.append(delta_time.total_seconds())
+            text = text.casefold()
 
-        time = datetime.now()
-        t3 = normalize_trans(text)
-        delta_time = datetime.now() - time
-        trans_times.append(delta_time.total_seconds())
+            regex_times = []
+            lower_times = []
+            trans_times = []
+            remove_times = []
 
-        time = datetime.now()
-        t4 = remove_punctuation_translate(text)
-        delta_time = datetime.now() - time
-        remove_times.append(delta_time.total_seconds())
+            for i in range(10):
 
-    print('regex, avg: {}\t std dev: {}'
-          .format(np.average(regex_times),np.std(regex_times)))
-    print('regex-lower,  avg: {}\t std dev: {}'
-          .format(np.average(lower_times),np.std(lower_times)))
-    print('translate-replace,  avg: {}\t std dev: {}'
-          .format(np.average(trans_times),np.std(trans_times)))
-    print('translate-remove,  avg: {}\t std dev: {}'
-          .format(np.average(remove_times),np.std(remove_times)))
+                time = datetime.now()
+                t1 = normalize_regex(text)
+                delta_time = datetime.now() - time
+                regex_times.append(delta_time.total_seconds())
 
+                time = datetime.now()
+                t2 = lower_regex(text)
+                delta_time = datetime.now() - time
+                lower_times.append(delta_time.total_seconds())
+
+                time = datetime.now()
+                t3 = normalize_trans(text)
+                delta_time = datetime.now() - time
+                trans_times.append(delta_time.total_seconds())
+
+                time = datetime.now()
+                t4 = remove_punctuation_translate(text)
+                delta_time = datetime.now() - time
+                remove_times.append(delta_time.total_seconds())
+
+            result['regex_times'] = regex_times
+            result['lower_times'] = lower_times
+            result['trans_times'] = trans_times
+            result['remove_times'] = remove_times
+
+            result['regex_avg'] = np.average(regex_times)
+            result['lower_avg'] = np.average(lower_times)
+            result['trans_avg'] = np.average(trans_times)
+            result['remove_avg'] = np.average(remove_times)
+
+            result['regex_std'] = np.std(regex_times)
+            result['lower_std'] = np.std(lower_times)
+            result['trans_std'] = np.std(trans_times)
+            result['remove_std'] = np.std(remove_times)
+
+            results.append(result)
+
+    for r in results:
+        print('name: {},\tlength: {},\tregex: {},\ttranslate: {}'
+              .format(r['name'],r['length'],r['regex_avg'],r['trans_avg']))
 
 if __name__ == '__main__':
     translate_vs_regex_compare()
