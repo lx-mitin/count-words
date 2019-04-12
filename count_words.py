99011adf9a03e63db6be55748f7d3b6e4a7c135a
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-04-12 16:25:00 +0400

    Change names

---
+++ count_words.py
@@ -0,0 +1,34 @@
+"""Count words"""
+import string
+from collections import defaultdict
+
+def count_words(text):
+
+    t = text.casefold()    
+    t = t.translate(t.maketrans('','', string.punctuation))
+    t = t.split()
+    
+    cw = defaultdict(int)
+    for w in t:
+        cw[w] += 1
+
+    return cw
+
+def test_run():
+    from datetime import datetime 
+    with open('./data-txt/roadside-picnic-ru.txt','r') as f:
+        t = f.read()
+        print('Start counting time: {}'.format(datetime.now()))
+        words = count_words(t)
+        print('End counting time: {}'.format(datetime.now()))
+        
+        print('Total number of words: {}'.format(sum(words.values())))
+        print('Number of unique words: {}'.format(len(words)))
+        
+        sorted_counts = sorted(words.items(), key=lambda p:p[1], reverse=True)
+        print('10 most popular words are:')
+        for word, count in sorted_counts[:10]:
+            print('{}:\t{}'.format(word, count))
+
+if __name__ == '__main__':
+    test_run()
---
+++ fetch_web_page.py
@@ -0,0 +1,45 @@
+"""Fetch a web page"""
+from requests import get as requests_get
+from bs4 import BeautifulSoup 
+from json import dump as json_dump
+from os import getcwd
+from string import punctuation
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk import pos_tag
+from nltk import ne_chunk
+from nltk.stem import WordNetLemmatizer
+
+# Fetch a web page
+p = requests_get('https://github.com/lx-mitin?tab=repositories&q=&type=public')
+
+
+# Get a list of repositories
+soup = BeautifulSoup(p.text,'lxml')
+rep_tags = soup.body.find_all('a', itemprop="name codeRepository")
+reps = [{'rep_name':r.string.strip(),
+              'rep_url':'https://github.com'+r.attrs['href']} 
+            for r in rep_tags]
+
+
+# Normalize & tokenize text, remove stop words, tag parts of speach,
+# tag named entities, reduce words to their root form
+
+translation_dictionaty = {p:' ' for p in punctuation}
+
+for r in reps:
+    name = r['rep_name'].casefold()
+    name = name.translate(name.maketrans(translation_dictionaty))
+    words = word_tokenize(name)
+    words = [w for w in words if w not in stopwords.words('english')]
+    structure = pos_tag(words)
+    structure = ne_chunk(structure)
+    r['rep_name'] = [(WordNetLemmatizer().lemmatize(s[0]),s[1]) for s in structure]
+
+rep_names = [r['rep_name'] for r in reps]
+
+with open(getcwd()+'/data/rep-names.json','w') as json_file:
+    json_dump(rep_names,json_file)
+
+    
+print('Repositories list is loaded to `/data/rep-names.json` file')
