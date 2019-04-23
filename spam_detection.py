449b532471032e258660f48f1d49ec40e2e535e8
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-04-23 07:39:12 +0400

    Add spam_detection.py

---
+++ spam_detection.py
@@ -0,0 +1,12 @@
+""" Spam detection """
+import pandas as pd
+
+def read_csv(file_address):
+    with open(file_address,'r') as f:
+        df = pd.read_csv(f)
+
+    return df
+
+if __name__ == '__main__':
+    df = read_csv('./data/spam.csv')
+    print(df.head(10))
