5019aaa68bc533439e2715b3d1c1fe1e0ba0240a
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-05-11 21:11:18 +0400

    Add nrows parameter. Fix typo. Show dimensionality

--- src/spam_detection.py
+++ src/spam_detection.py
@@ -1,18 +1,19 @@
 """ Spam detection """
 import pandas as pd
 
-def read_csv(file_address):
+def read_csv(file_address,nrows=None):
     with open(file_address,'r') as f:
-        df = pd.read_csv(f)
+        df = pd.read_csv(f,nrows=nrows)
 
     return df
 
 if __name__ == '__main__':
-    df = read_csv('./data/csv/spam.csv')
+    df = read_csv('../data/csv/spam.csv',5)
 
     df = df.drop(df.columns[[2,3,4]],1)
     df = df.rename(columns={'v1':'category','v2':'text'})
 
     df['category'] = df['category'].map({'ham':0,'spam':1})
 
-    print(df.head(10))
+    print(df.shape)
+    print(df.head())
