2d5ad1c42e1979f0d8a1fb8a33b97f25514fb10e
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-05-18 12:48:15 +0400

    Load only needed columns rather than remove unnecessary columns later

--- src/spam_detection.py
+++ src/spam_detection.py
@@ -1,18 +1,23 @@
 """ Spam detection """
 import pandas as pd
 
-def read_csv(file_address,nrows=None):
+def read_csv(file_address,names=None,usecols=None,nrows=None):
     with open(file_address,'r') as f:
-        df = pd.read_csv(f,nrows=nrows)
+        df = pd.read_csv(f,names=names,usecols=usecols,nrows=nrows)
 
     return df
 
 if __name__ == '__main__':
-    df = read_csv('../data/csv/spam.csv',5)
 
-    df = df.drop(df.columns[[2,3,4]],1)
-    df = df.rename(columns={'v1':'category','v2':'text'})
-
+    # Load useful info sample
+    df = read_csv(
+                file_address='../data/csv/spam.csv',
+                names=['category','text'],
+                usecols=[0,1],
+                nrows=5
+                )
+    
+    # Prepare for processing
     df['category'] = df['category'].map({'ham':0,'spam':1})
 
     print(df.shape)
