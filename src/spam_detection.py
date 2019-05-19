fe7170d10c5c373f60c34baa4bd1accab272a604
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-05-19 21:04:48 +0400

    Improve readability

--- src/spam_detection.py
+++ src/spam_detection.py
@@ -1,24 +1,22 @@
 """ Spam detection """
 import pandas as pd
 
-def read_csv(file_address,names=None,usecols=None,nrows=None):
+def read_csv(file_address, names=None, usecols=None, nrows=None):
     with open(file_address,'r') as f:
-        df = pd.read_csv(f,names=names,usecols=usecols,nrows=nrows)
+        df = pd.read_csv(f, names=names, usecols=usecols, nrows=nrows)
 
     return df
 
 if __name__ == '__main__':
 
     # Load useful info sample
-    df = read_csv(
-                file_address='../data/csv/spam.csv',
-                names=['category','text'],
-                usecols=[0,1],
-                nrows=5
-                )
-    
+    df = read_csv(file_address='../data/csv/spam.csv',
+                  names=['category', 'text'],
+                  usecols=[0, 1],
+                  nrows=5)
+
     # Prepare for processing
-    df['category'] = df['category'].map({'ham':0,'spam':1})
+    df['category'] = df['category'].map({'ham': 0, 'spam': 1})
 
     print(df.shape)
     print(df.head())
