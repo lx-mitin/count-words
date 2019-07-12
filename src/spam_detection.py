71041a1e95ecfca9603c55de76600285d7c93a75
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-07-12 19:50:37 +0400

    Rearrange code

--- src/spam_detection.py
+++ src/spam_detection.py
@@ -16,62 +16,60 @@ def read_csv(file_address, names=None, usecols=None, skiprows=0, nrows=None):
             usecols=usecols,
             skiprows=skiprows,
             nrows=nrows
-        )
+            )
 
     return df
 
 
-if __name__ == '__main__':
-
-    # Load spam samples
-    df = read_csv(
-        file_address='../data/csv/spam.csv',
-        names=['category', 'text'],
-        usecols=[0, 1],
-        skiprows=1,
-        )
+# Load spam samples
+df = read_csv(
+    file_address='../data/csv/spam.csv',
+    names=['category', 'text'],
+    usecols=[0, 1],
+    skiprows=1,
+    )
 
 
-    # Prepare categories for processing
-    df['category'] = df['category'].map({'ham': 0, 'spam': 1})
+# Prepare categories for processing
+df['category'] = df['category'].map({'ham': 0, 'spam': 1})
 
-    print(df.shape)
-    print(df.head())
-
-
-    # Split data into training and testing set
-    split = train_test_split(
-        df['text'],
-        df['category'],
-        test_size=0.3
-        )
-
-    training_words, testing_words, training_cat, testing_cat = split
+print(df.shape)
+print(df.head())
 
 
-    # Extract features (create Bag of Words)
-    count_vector = CountVectorizer(
-        lowercase=True, 
-        stop_words=stopwords.words('english')
-        )
-    training_set = count_vector.fit_transform(training_words)
-    testing_set = count_vector.transform(testing_words)
+# Split data into training and testing set
+split = train_test_split(
+    df['text'],
+    df['category'],
+    test_size=0.3
+    )
 
-    print('Training set {}'.format(training_set.shape))
-    print('Testing set {}'.format(testing_set.shape))
+training_words, testing_words, training_cat, testing_cat = split
 
 
-    # Create model using multinomial naive bayes
-    naive_bayes_model = MultinomialNB(alpha=0.5)
-    naive_bayes_model.fit(training_set, training_cat)
+# Extract features (create Bag of Words)
+count_vector = CountVectorizer(
+    lowercase=True, 
+    stop_words=stopwords.words('english')
+    )
+training_set = count_vector.fit_transform(training_words)
+testing_set = count_vector.transform(testing_words)
+
+print('Training set {}'.format(training_set.shape))
+print('Testing set {}'.format(testing_set.shape))
 
 
-    # Make predictions for testing data
-    predictions = naive_bayes_model.predict(testing_set)
+# Create model using multinomial naive bayes
+naive_bayes_model = MultinomialNB(alpha=0.5)
+naive_bayes_model.fit(training_set, training_cat)
 
 
-    # Verify model quality
-    print('Accuracy: {}'.format(accuracy_score(testing_cat, predictions)))
-    print('Precision: {}'.format(precision_score(testing_cat, predictions)))
-    print('Recall: {}'.format(recall_score(testing_cat, predictions)))
-    print('f1: {}'.format(f1_score(testing_cat, predictions)))
+# Make predictions for testing data
+predictions = naive_bayes_model.predict(testing_set)
+
+
+# Verify model quality
+print('Accuracy: {}'.format(accuracy_score(testing_cat, predictions)))
+print('Precision: {}'.format(precision_score(testing_cat, predictions)))
+print('Recall: {}'.format(recall_score(testing_cat, predictions)))
+print('f1: {}'.format(f1_score(testing_cat, predictions)))
