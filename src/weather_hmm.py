bca2d0a9bb499fda2fa2e7f3178b288d5f1771fd
lx.mitin@gmail.com <lx.mitin@gmail.com>
2019-07-13 19:29:16 +0400

    Add transitions, finalize topology

--- src/weather_hmm.py
+++ src/weather_hmm.py
@@ -10,3 +10,17 @@ rainy_emissions = DiscreteDistribution({'yes': 0.8, 'no': 0.2})
 rainy_state = State(rainy_emissions, name='rainy')
 
 model.add_states(sunny_state, rainy_state)
+
+model.add_transition(model.start, sunny_state, 0.5)
+model.add_transition(model.start, rainy_state, 0.5)
+
+model.add_transition(sunny_state, rainy_state, 0.2)
+model.add_transition(sunny_state, sunny_state, 0.8)
+
+model.add_transition(rainy_state, rainy_state, 0.6)
+model.add_transition(rainy_state, sunny_state, 0.4)
+
+model.bake()
+
+print(model.edge_count())
+print(model.node_count())
